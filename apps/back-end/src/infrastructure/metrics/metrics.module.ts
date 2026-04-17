26bb768515e1a6d34c18648fd58bbb28dbf65ebb
Jefferson Estevam <jesbmail@gmail.com>
2026-04-17 00:42:10 -0300

    feat(metrics): add http and agent custom prometheus metrics

--- apps/back-end/src/infrastructure/metrics/metrics.module.ts
+++ apps/back-end/src/infrastructure/metrics/metrics.module.ts
@@ -1,7 +1,12 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { HttpMetricsMiddleware } from './http-metrics.middleware.js';
 import { MetricsController } from './metrics.controller.js';
 
 @Module({
   controllers: [MetricsController],
 })
-export class MetricsModule {}
+export class MetricsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(HttpMetricsMiddleware).forRoutes('*');
+  }
+}
